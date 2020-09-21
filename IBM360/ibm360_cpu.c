68e67b719d592f3bb0caa98c5120242660beb0c3
Richard Cornwell <rcornwell@github.com>
2020-09-21 19:10:17 -0400

    IBM360: Fixed bug in CVD instruction.

--- IBM360/ibm360_cpu.c
+++ IBM360/ibm360_cpu.c
@@ -2734,7 +2734,7 @@ save_dbl:
                 while (dest != 0) {  /* Convert digits until zero */
                     int d = dest % 10;
                     dest /= 10;
-                    if (temp > 32)
+                    if (temp >= 32)
                         src1 |= (d << (temp - 32));
                     else
                         src1h |= (d << temp);
