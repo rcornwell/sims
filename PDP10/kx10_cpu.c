bad641393e5868bd5bfb5eb9ffa5f56562e86b6e
Lars Brinkhoff <lars@nocrew.org>
2021-07-19 13:01:38 +0200

    KA10: Change order of AUXCPU and TEN11 check.
    The PDP-6 memory should have precedence, shadowing TEN11 if they overlap.

--- PDP10/kx10_cpu.c
+++ PDP10/kx10_cpu.c
@@ -3410,6 +3410,15 @@ int Mem_read_its(int flag, int cur_context, int fetch, int mod) {
     } else {
         if (!page_lookup_its(AB, flag, &addr, 0, cur_context, fetch, mod))
             return 1;
+#if NUM_DEVS_AUXCPU > 0
+        if (AUXCPURANGE(addr) && QAUXCPU) {
+            if (auxcpu_read (addr, &MB)) {
+                nxm_flag = 1;
+                check_apr_irq();
+                return 1;
+            }
+        }
+#endif
 #if NUM_DEVS_TEN11 > 0
         if (T11RANGE(addr) && QTEN11) {
             if (ten11_read (addr, &MB)) {
@@ -3418,15 +3427,6 @@ int Mem_read_its(int flag, int cur_context, int fetch, int mod) {
                 return 1;
             }
             return 0;
-        }
-#endif
-#if NUM_DEVS_AUXCPU > 0
-        if (AUXCPURANGE(addr) && QAUXCPU) {
-            if (auxcpu_read (addr, &MB)) {
-                nxm_flag = 1;
-                check_apr_irq();
-                return 1;
-            }
         }
 #endif
         if (addr >= MEMSIZE) {
