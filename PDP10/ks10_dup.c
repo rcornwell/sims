0a6e6f00648894700e996e2873a61f9f9c67eb20
Richard Cornwell <rich@sky-visions.com>
2022-02-07 22:14:18 -0500

    KA10: Cleanup KS10 DUP code to address register properly.

--- PDP10/ks10_dup.c
+++ PDP10/ks10_dup.c
@@ -470,8 +470,10 @@ int32  orig_val;
 
 if ((dptr->units[0].flags & UNIT_DIS) != 0)
     return 1;
-PA &= dibp->uba_mask;
 dup = ((PA - dup_dib.uba_addr) >> 3);                   /* get line num */
+PA &= dibp->uba_mask;
+sim_debug(DEBUG_DETAIL, DUPDPTR, "dup_rd(PA=%010o [%s], data=0x%X) ", PA, dup_rd_regs[(PA >> 1) & 03], *data);
+sim_debug_bits(DEBUG_DETAIL, DUPDPTR, bitdefs[(PA >> 1) & 03], (uint32)(orig_val), (uint32)(regs[(PA >> 1) & 03][dup]), TRUE);
 
 if (dup >= dup_desc.lines)                              /* validate line number */
     return 1;
@@ -501,7 +503,7 @@ switch ((PA >> 1) & 03) {                               /* case on PA<2:1> */
         break;
     }
 
-sim_debug(DEBUG_DETAIL, DUPDPTR, "dup_rd(PA=0x%08X [%s], data=0x%X) ", PA, dup_rd_regs[(PA >> 1) & 03], *data);
+sim_debug(DEBUG_DETAIL, DUPDPTR, "dup_rd(PA=%010o [%s], data=0x%X) ", PA, dup_rd_regs[(PA >> 1) & 03], *data);
 sim_debug_bits(DEBUG_DETAIL, DUPDPTR, bitdefs[(PA >> 1) & 03], (uint32)(orig_val), (uint32)(regs[(PA >> 1) & 03][dup]), TRUE);
 
 return 0;
@@ -515,10 +517,12 @@ struct pdp_dib   *dibp = (DIB *)dptr->ctxt;
 int32  dup;
 int32  orig_val;
 
+sim_debug(DEBUG_DETAIL, DUPDPTR, "dup_wr(PA=%010o [%s], data=0x%X) ", PA, dup_wr_regs[(PA >> 1) & 03], data);
+sim_debug_bits(DEBUG_DETAIL, DUPDPTR, bitdefs[(PA >> 1) & 03], (uint32)orig_val, (uint32)regs[(PA >> 1) & 03][dup], TRUE);
 if ((dptr->units[0].flags & UNIT_DIS) != 0)
     return 1;
-PA &= dibp->uba_mask;
 dup = ((PA - dup_dib.uba_addr) >> 3);                   /* get line num */
+PA &= dibp->uba_mask;
 
 if (dup >= dup_desc.lines)                              /* validate line number */
     return 1;
@@ -623,8 +627,6 @@ switch ((PA >> 1) & 03) {                               /* case on PA<2:1> */
         break;
     }
 
-sim_debug(DEBUG_DETAIL, DUPDPTR, "dup_wr(PA=0x%08X [%s], data=0x%X) ", PA, dup_wr_regs[(PA >> 1) & 03], data);
-sim_debug_bits(DEBUG_DETAIL, DUPDPTR, bitdefs[(PA >> 1) & 03], (uint32)orig_val, (uint32)regs[(PA >> 1) & 03][dup], TRUE);
 dup_get_modem (dup);
 return 0;
 }
